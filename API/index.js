78d65391d3df83c88a60f3051a33042f992c7257
Gabriel <gabriel99luques@gmail.com>
2021-10-20 21:09:02 -0300

    connecting to MongoDB

--- API/index.js
+++ API/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //create the server
 const app = express();
+
+//Connect to mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/budget',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
 
 //port and start the server
 app.listen(4000, () =>{
