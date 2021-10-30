42bff708387ca999a76a7888b6e5b157f490501e
Gabriel <gabriel99luques@gmail.com>
2021-10-29 21:45:26 -0300

    check API with Axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+//we import the customer from Axios
+import clientAxios from './config/axios';
 
 //components
 import Users from './components/Users';
 import NewEntry from './components/Users';
 import Appointment from './components/Users';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  //State de la app
+  const [budget,saveBudget] = useState([]);
+
+  useEffect( () =>{
+    const consultAPI = () =>{
+      clientAxios.get('/users')
+        .then(respuesta => {
+          console.log(respuesta)
+        })
+        .catch(error =>{
+          console.log(error);
+        })
+    }
+    consultAPI();
+  }, [] );
 
   console.log(procces.env.REACT_APP_BACKEND_URL);
   return (
